91b81849cc0dec55ac166787acf0cf2a6e3cd8f5
AlammobaDar <162420273+alammobaDar@users.noreply.github.com>
2026-02-04 00:35:34 +0800

    add docker set up for local mongodb configuration

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 from fastapi import FastAPI
+from fastapi.exceptions import HTTPException
+from contextlib import asynccontextmanager
+from src.ventph_api.infrastructure.database.mongo import client
 
-app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "IT WORKS!"}
+# Health check
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    try:
+        await client.admin.command("ping")
+        print("✅ MongoDB connected successfully!")
+    except Exception as e:
+        print(f"❌ MongoDB connection failed: {e}")
+
+    yield
+
+    client.close()
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/health")
+async def health_check():
+    try:
+        # Pings the database
+        await client.admin.command("ping")
+        return {"status": "online", "database": "connected"}
+    except Exception as e:
+        raise HTTPException(status_code=503, detail=f"Database unreachable: {str(e)}")
---
+++ src/ventph_api/config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    MONGO_LOCAL_URL: str
+    MONGODB_DB_NAME: str
+
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    LLM_API_KEY: str
+    LLM_ENDPOINT: str
+
+    class Config:
+        env_file = ".env"
+
+
+env = Settings()
